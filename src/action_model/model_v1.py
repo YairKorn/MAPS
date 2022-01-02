107382e01612335df7b22e7fde1859c7ad83b880
YairKorn <yair.korn1@gmail.com>
2022-01-02 23:11:44 +0200

    W#13

--- src/action_model/model_v1.py
+++ src/action_model/model_v1.py
@@ -12,23 +12,25 @@ class ActionModel():
         self.n_agents = args.n_agents
         self.device = "cuda" if args.use_cuda else "cpu"
 
-        # Buffer for storing episodes until the results of the action are revealed (by the next perception)
-        #! need to implement in stochastic env (v2/v3)
-        self.state_buffer = deque()
-
-        # Buffer for sequential single-agent samples (rather than n-agents samples) #$ (Fits to dynamic CG)
+        # Buffers for store learning episodes (because the default buffer is not fit)
         model_scheme = {
             "obs": scheme["obs"],
             "actions": scheme["actions"],
             "avail_actions": scheme["avail_actions"],
-            "new_obs": scheme["obs"],
+            # "new_obs": scheme["obs"],
             "reward": scheme["reward"],
             "terminated": scheme["terminated"],
         }
         groups = {'agents': self.n_agents}
         self.episode_limit = args.env_args['episode_limit']
+
+        # Buffer for storing episodes until the results of the action are revealed (by the next perception)
+        #! need to implement in stochastic env (v2/v3)
+        #* self.temp_buffer = ReplayBuffer(model_scheme, groups, args.buffer_size, self.n_agents, device=self.device)
+
+        # Buffer for sequential single-agent samples (rather than n-agents samples) #$ (Fits to dynamic CG)
         self.buffer = ReplayBuffer(model_scheme, groups, args.buffer_size, self.n_agents * self.episode_limit + 1, device=self.device)
-        pass
+
 
     """ When new perception is percepted, update the real state """
     def update_state(self, state):
@@ -36,9 +38,19 @@ class ActionModel():
         return np.arange(self.n_agents)
 
     """ Update the state based on an action """
-    def step(self, agent_id, action):
-        prev_state = deepcopy(self.state)
-        self.state = self._apply_action_on_state(self.state, action)
+    def step(self, agent_id, action, obs, avail_actions):
+        self.state, reward, terminated = self._apply_action_on_state(self.state, action)
+        
+        action_data = {
+            "obs": obs,
+            "avail_actions": avail_actions,
+            "actions": action * self._one_hot((1, self.n_agents), (agent_id,)),
+            "reward": reward,
+            "terminated": terminated
+        }
+        self.buffer.update(action_data, ts=t)
+
+
         
         #! Temporary buffer until new perception from the environment is gathered
         # buffer_entry = BufferEntry(agent_id, prev_state, action, deepcopy(self.state), 0)
@@ -65,3 +77,8 @@ class ActionModel():
     def _detect_interaction(self, state):
         return np.arange(self.n_agents)
 
+    @staticmethod
+    def _one_hot(shape, one_hot):
+        arr = np.zeros(shape)
+        arr[one_hot] = 1
+        return arr

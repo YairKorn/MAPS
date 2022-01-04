20644bb76b818a982f891075fb8312d7678ec8fc
YairKorn <yair.korn1@gmail.com>
2022-01-04 18:52:10 +0200

    W#13

--- src/action_model/model_v1.py
+++ src/action_model/model_v1.py
@@ -1,16 +1,12 @@
 import numpy as np
-import torch as th
-from copy import deepcopy
-from collections import namedtuple, deque
 from components.episode_buffer import ReplayBuffer
-
-BufferEntry = namedtuple('BufferEntry', ['obs', 'action', 'avail_actions', 'new_obs', 'reward'])
 
 """ Basic action model class - action models inherit from it """
 class ActionModel():
     def __init__(self, scheme, args) -> None:
         self.n_agents = args.n_agents
         self.device = "cuda" if args.use_cuda else "cpu"
+        self.t = 0
 
         # Buffers for store learning episodes (because the default buffer is not fit)
         model_scheme = {
@@ -28,54 +24,48 @@ class ActionModel():
         #! need to implement in stochastic env (v2/v3)
         #* self.temp_buffer = ReplayBuffer(model_scheme, groups, args.buffer_size, self.n_agents, device=self.device)
 
-        # Buffer for sequential single-agent samples (rather than n-agents samples) #$ (Fits to dynamic CG)
+        # Buffer for sequential single-agent samples (rather than n-agents samples) #* (Fits to dynamic CG)
         self.buffer = ReplayBuffer(model_scheme, groups, args.buffer_size, self.n_agents * self.episode_limit + 1, device=self.device)
 
 
     """ When new perception is percepted, update the real state """
     def update_state(self, state):
         self.state = state
-        return np.arange(self.n_agents)
+        return self._detect_interaction()
+
 
     """ Update the state based on an action """
     def step(self, agent_id, action, obs, avail_actions):
         self.state, reward, terminated = self._apply_action_on_state(self.state, action)
         
-        action_data = {
+        transition_data = {
             "obs": obs,
             "avail_actions": avail_actions,
             "actions": action * self._one_hot((1, self.n_agents), (agent_id,)),
             "reward": reward,
             "terminated": terminated
         }
-        self.buffer.update(action_data, ts=t)
-
-
         
-        #! Temporary buffer until new perception from the environment is gathered
-        # buffer_entry = BufferEntry(agent_id, prev_state, action, deepcopy(self.state), 0)
-        # self.buffer.append(buffer_entry)
+        self.buffer.update(transition_data, ts=self.t)
+        self.t += 1
+
+        if terminated:
+            self.buffer.update({
+                "obs": self.state
+            }, ts=self.t)
 
 
-    # Use the general state to create a partial-observability observation for the agents
-    def _build_observation(self, i):
-        if self.full_observable:
-            return deepcopy(self.state)
+    """ Use the general state to create a partial-observability observation for the agents """
+    def get_obs_agent(self, agent_id):
+        raise NotImplementedError
 
-        #! build
-
-
+    """ Simulate the result of action in the environment """
     def _apply_action_on_state(self, state, action, result=0):
         raise NotImplementedError
 
-
-    def get_obs_agent(self, agent_id):
-        pass
-
-
     """ This function build a dynamic CG using pre-defined (problem specific) model """
-    def _detect_interaction(self, state):
-        return np.arange(self.n_agents)
+    def _detect_interaction(self):
+        return np.arange(self.n_agents) # by default, no interaction
 
     @staticmethod
     def _one_hot(shape, one_hot):
